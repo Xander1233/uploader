b7416209231f55b0a1256e61b6b7e320b4c77a86
David Neidhart <david.neidhart@gmx.net>
2023-11-30 18:26:48 +0100

    feat: hello world web server
    Signed-off-by: David Neidhart <david.neidhart@gmx.net>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    let _rocket = rocket::build()
+        .mount("/", routes![index])
+        .launch()
+        .await;
+
+    Ok(())
 }
