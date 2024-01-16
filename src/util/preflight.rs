fd84ba2f94637d341af772d3db24a7ac0dd90505
David Neidhart <david.neidhart@gmx.net>
2024-01-16 16:20:18 +0100

    feat: add password support
    Signed-off-by: David Neidhart <david.neidhart@gmx.net>

--- src/util/preflight.rs
+++ src/util/preflight.rs
@@ -67,15 +67,11 @@ pub async fn preflight(client: &Client) {
 
     client
         .query(
-            "CREATE TABLE IF NOT EXISTS viewtokens ( \
+            "CREATE TABLE IF NOT EXISTS view_tokens ( \
         id text PRIMARY KEY, \
-        userid text, \
         fileid text, \
         token text, \
         created_at TIMESTAMP NOT NULL DEFAULT NOW(), \
-        CONSTRAINT fk_id \
-            FOREIGN KEY (userid) \
-                REFERENCES users(id) ON DELETE CASCADE, \
         CONSTRAINT fk_fileid \
             FOREIGN KEY (fileid) \
                 REFERENCES files(id) ON DELETE CASCADE \
